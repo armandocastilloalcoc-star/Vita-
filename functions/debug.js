9473208c29f851289aa68cb03f557d391b5d8a4e
armandocastilloalcoc-star <armandocastilloalcoc@gmail.com>
2026-06-13 07:09:09 -0600

    Delete functions/debug.js

--- functions/debug.js
+++
@@ -1,6 +0,0 @@
-export const config = { path: ["/debug"] };
-export default async () => new Response(JSON.stringify({
-  hasDb: !!process.env.NETLIFY_DATABASE_URL,
-  dbKeys: Object.keys(process.env).filter(k => /DATABASE|NEON|PG/i.test(k)),
-  hasJwt: !!process.env.JWT_SECRET
-}, null, 2), { headers: { "Content-Type": "application/json" } });
